05891d919d2daf99849d4fbbc8651ef4bd8cb405
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2025-10-01 18:09:08 +0300

    hello world

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        pythonEnv = pkgs.python312.withPackages (
+          ps: with ps; [
+            numpy
+            scipy
+          ]
+        );
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pythonEnv
+            pyright
+            ruff
+            nixd
+          ];
+        };
+      }
+    );
+}
